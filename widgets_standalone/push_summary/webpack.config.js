2e1f35e29671d515e3636f008d64a6242950d89a
Matt Herold <mattherold@icloud.com>
2024-06-02 23:28:28 +0200

    dist added

--- widgets_standalone/push_summary/webpack.config.js
+++ widgets_standalone/push_summary/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     	}
 
     }},
-  mode: "development",
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   experiments: {
     outputModule: true,
   },
